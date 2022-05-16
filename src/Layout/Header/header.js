bcbe146953685e4f8bd322f38ba6041f867b5e6b
nguyenngochieu <ngnohieu@gmail.com>
2022-05-16 10:53:30 +0700

    1605

--- src/Layout/Header/header.js
+++ src/Layout/Header/header.js
@@ -1,13 +1,76 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "../../App";
 import LoginModel from "../../Components/LogIn";
 import SignOnModal from "../../Components/SignOn";
+import API, { endpoints } from "../../configs/API";
 import "./styleheader.css";
 
 const Header = (props) => {
    const [openModalSignIn, setOpenModalSignIn] = useState(false);
    const [openModalSignOn, setOpenModalSignOn] = useState(false);
 
+   const [clientId, setClientId] = useState();
+   const [clientSecret, setClientSecret] = useState();
+
+   const [user, dispatch] = useContext(UserContext);
+
+   useEffect(() => {
+      const loadClientKey = async () => {
+         const res = await API.get(endpoints["oauth2_info"]);
+         setClientId(res.data.client_id);
+         setClientSecret(res.data.client_secret);
+      };
+      loadClientKey();
+   }, []);
+
+   let replaceRightPart = //phan nay la default right part header
+      (
+         <>
+            <a
+               href="#"
+               className="login-right-header hover"
+               onClick={() => {
+                  setOpenModalSignIn(true);
+               }}
+            >
+               Log in
+            </a>
+            <a
+               href="#"
+               className="signout-right-header hover"
+               onClick={() => {
+                  setOpenModalSignOn(true);
+               }}
+            >
+               Sign on
+            </a>
+         </>
+      );
+   if (user != null) {
+      replaceRightPart = (
+         <>
+            <a
+               href="#"
+               className="login-right-header hover"
+               onClick={() => {
+                  setOpenModalSignIn(true);
+               }}
+            >
+               Username
+            </a>
+            <a
+               href="#"
+               className="signout-right-header hover"
+               onClick={() => {
+                  setOpenModalSignOn(true);
+               }}
+            >
+               Avatar
+            </a>
+         </>
+      );
+   }
    return (
       <>
          <div className="header flex">
@@ -24,26 +87,7 @@ const Header = (props) => {
                   <img src={props.logo} />
                </Link>
             </div>
-            <div className="right-header flex ">
-               <a
-                  href="#"
-                  className="login-right-header hover"
-                  onClick={() => {
-                     setOpenModalSignIn(true);
-                  }}
-               >
-                  Log in
-               </a>
-               <a
-                  href="#"
-                  className="signout-right-header hover"
-                  onClick={() => {
-                     setOpenModalSignOn(true);
-                  }}
-               >
-                  Sign on
-               </a>
-            </div>
+            <div className="right-header flex ">{replaceRightPart}</div>
          </div>
          {openModalSignIn && <LoginModel closeModal={setOpenModalSignIn} />}
          {openModalSignOn && <SignOnModal closeModal={setOpenModalSignOn} />}
---
+++ src/Reducers/MyReducer.js
@@ -0,0 +1,11 @@
+const myReducer = (user, action) => {
+   switch (action.type) {
+      case "login":
+         return action.payload;
+      case "logout":
+         return null;
+   }
+   return user;
+};
+
+export default myReducer;
