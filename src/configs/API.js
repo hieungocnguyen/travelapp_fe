ad5466a27255b28aa79d95ee09a1d1ce7b5ed5f7
nguyenngochieu <ngnohieu@gmail.com>
2022-05-16 13:15:36 +0700

    loginApi165

--- src/configs/API.js
+++ src/configs/API.js
@@ -1,11 +1,20 @@
 import axios from "axios";
+import cookies from "react-cookies";
 
 export let endpoints = {
    users: "/users/",
-   user_current: "/users/current_user/",
+   current_user: "/users/current_user/",
    oauth2_info: "/oauth2_info/",
    login: "/o/token/",
 };
+
+export const authAxios = () =>
+   axios.create({
+      baseURL: "http://localhost:8000",
+      headers: {
+         Authorization: `Bearer ${cookies.load("access_token")}`,
+      },
+   });
 
 export default axios.create({
    baseURL: "http://localhost:8000",
